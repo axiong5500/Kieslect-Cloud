e2fa920daf7ae1022263a5b2179e235e46ac2b9c
A雄 <870053231@qq.com>
2024-05-30 18:05:16 +0800

    1.完善获取App 配置表接口

--- kieslect-modules/kieslect-device/src/main/java/com/kieslect/device/controller/AppManageController.java
+++ kieslect-modules/kieslect-device/src/main/java/com/kieslect/device/controller/AppManageController.java
@@ -1,10 +1,13 @@
 package com.kieslect.device.controller;
 
 import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.json.JSONUtil;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.kieslect.common.core.domain.R;
 import com.kieslect.device.domain.AppManage;
 import com.kieslect.device.domain.AppPackeageManage;
+import com.kieslect.device.domain.ParamConfig;
 import com.kieslect.device.domain.vo.AppManageVO;
 import com.kieslect.device.domain.vo.AppPackeageManageVO;
 import com.kieslect.device.service.IAppManageService;
@@ -22,7 +25,7 @@ import java.util.stream.Collectors;
 
 /**
  * <p>
- *  前端控制器
+ * 前端控制器
  * </p>
  *
  * @author kieslect
@@ -44,16 +47,16 @@ public class AppManageController {
     IParamConfigService paramConfigService;
 
     @GetMapping("/getApp")
-    public R<?> getAppManageObj(@RequestParam(value = "appMark", required = false)String appMark) {
-        Map<String,Object> result = new HashMap<>();
+    public R<?> getAppManageObj(@RequestParam(value = "appMark", required = false) String appMark) {
+        Map<String, Object> result = new HashMap<>();
         AppManageVO appManageVO = new AppManageVO();
 
 
         QueryWrapper<AppManage> queryWrapper = new QueryWrapper<>();
-        queryWrapper.eq("app_mark",appMark);
-        AppManage appManage =   appManageService.getOne(queryWrapper);
+        queryWrapper.eq("app_mark", appMark);
+        AppManage appManage = appManageService.getOne(queryWrapper);
 
-        if (appManage == null){
+        if (appManage == null) {
             result.put("appObj", null);
             return R.ok(result);
         }
@@ -63,12 +66,11 @@ public class AppManageController {
         List<AppPackeageManage> appPackeageManageList = appPackeageManageService.list();
 
 
-
         // 使用 stream 根据 appid 分组
         Map<Integer, List<AppPackeageManageVO>> appPackageManageMap = appPackeageManageList.stream()
                 .map(appPackeageManage -> {
                     AppPackeageManageVO appPackeageManageVO = new AppPackeageManageVO();
-                    BeanUtil.copyProperties(appPackeageManage,appPackeageManageVO);
+                    BeanUtil.copyProperties(appPackeageManage, appPackeageManageVO);
 
                     appPackeageManageVO.setVersionCode(Integer.valueOf(appPackeageManage.getVersion()));
                     appPackeageManageVO.setVersionName(appPackeageManage.getAppVersion());
@@ -78,26 +80,60 @@ public class AppManageController {
                 .collect(Collectors.groupingBy(AppPackeageManageVO::getAppId));
 
 
-        Map<String,Object> params = new HashMap<>();
-        params.put("web_agreement",7);
-        params.put("web_privacy_policy",8);
+        Map<String, Object> params = getParamsMap(appManage);
 
-        BeanUtil.copyProperties(appManage,appManageVO);
+        BeanUtil.copyProperties(appManage, appManageVO);
         appManageVO.setAppPackages(appPackageManageMap.get(appManage.getId()) == null ? new ArrayList<>() : appPackageManageMap.get(appManage.getId()));
         appManageVO.setParams(params);
         result.put("appObj", appManageVO);
 
         return R.ok(result);
     }
+
+    private Map<String, Object> getParamsMap(AppManage appManage) {
+        Map<String, Object> params = new HashMap<>();
+        String paramCollection = appManage.getParamCollection();
+        if (StrUtil.isNotBlank(paramCollection)) {
+            //所有参数
+            List<ParamConfig> paramConfigs = paramConfigService.list();
+
+            // 将 paramCollection 的 JSON 字符串转换为 Map
+            Map<String, String> paramCollectionMap = JSONUtil.toBean(paramCollection, Map.class);
+
+            // 创建 ID 到 paramName 的映射
+            Map<String, String> paramConfigMap = paramConfigs.stream()
+                    .collect(Collectors.toMap(
+                            paramConfig -> paramConfig.getId().toString(),
+                            ParamConfig::getParamName
+                    ));
+
+            // 遍历 paramCollectionMap，将 ID 替换为 paramName
+            for (Map.Entry<String, String> entry : paramCollectionMap.entrySet()) {
+                String key = entry.getKey();
+                String value = entry.getValue();
+                if (paramConfigMap.containsKey(key)) {
+                    // 将 ID 替换为 paramName
+                    params.put(paramConfigMap.get(key), value);
+                } else {
+                    // 如果没有找到对应的 paramName，忽略
+                }
+            }
+
+        }
+        return params;
+    }
+
     @GetMapping("/sys/getList")
     public R<?> sysGetAppManageList() {
         return R.ok(appManageService.list());
     }
+
     // update
     @PostMapping("/sys/update")
     public R<?> updateAppManage(@RequestBody AppManage appManage) {
         return R.ok(appManageService.updateById(appManage));
     }
+
     // delete
     @PostMapping("/sys/delete")
     public R<?> deleteAppManage(@RequestBody AppManage appManage) {
